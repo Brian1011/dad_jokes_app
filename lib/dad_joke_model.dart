51e2e043a306ecbc676d879ae301c6475e0d31c0
Brian Mutinda <mutindabrian26@gmail.com>
2021-11-14 18:58:08 +0300

    display in cards

--- lib/dad_joke_model.dart
+++ lib/dad_joke_model.dart
@@ -9,13 +9,12 @@ DadJoke dadJokeFromMap(String str) => DadJoke.fromMap(json.decode(str));
 String dadJokeToMap(DadJoke data) => json.encode(data.toMap());
 
 class DadJoke {
-  DadJoke({
-    this.id,
-    this.joke,
-  });
+  DadJoke({this.id, this.joke, this.viewed = false, this.liked});
 
   String? id;
   String? joke;
+  bool viewed = false;
+  bool? liked;
 
   factory DadJoke.fromMap(Map<String, dynamic> json) => DadJoke(
         id: json["id"] == null ? null : json["id"],
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dad_jokes/services/jokes_service.dart';
 import 'package:flutter/material.dart';
+import 'package:tcard/tcard.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,11 +8,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  jokeCards(context) {
+    return List.generate(
+      jokesService.dadJokes.length,
+      (index) => Container(
+        height: MediaQuery.of(context).size.height * 0.75,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 17),
+              blurRadius: 23.0,
+              spreadRadius: -13.0,
+              color: Colors.black54,
+            )
+          ],
+        ),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: Center(
+          child: Text(
+            "${jokesService.dadJokes[index].joke}",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text('white'),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+            color: Colors.grey[800],
+            height: double.infinity,
+            width: double.infinity,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              TCard(cards: jokeCards(context)),
+            ])),
+        bottomNavigationBar: Container(
+          color: Colors.black,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.home,
+                  size: 32,
+                ),
+                color: Colors.white,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border),
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
